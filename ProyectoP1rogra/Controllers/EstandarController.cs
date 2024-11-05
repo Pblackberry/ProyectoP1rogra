b26a386d019b352ef9d9a5203829043688969a2a
Pblackberry <tomaslopezandrade2004@gmail.com>
2024-11-04 20:49:12 -0500

    Filtrado listo

---
+++ ProyectoP1rogra/Controllers/EstandarController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoP1rogra.Controllers
+{
+    public class EstandarController : Controller
+    {
+        private readonly GymContext _context;
+        public EstandarController(GymContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Estandar()
+        {
+            var Estandar = await _context.Membresias
+            .Where(e => e.membresia == "Estandar")
+            .ToListAsync();
+            return View(Estandar);
+        }
+    }
+}
